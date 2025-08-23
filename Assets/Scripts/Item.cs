c057b8552464fb33218a00d72617ce6f276bb696
Timb9o <timo.patolla@hotmail.de>
2025-08-23 19:37:52 +0200

    Added ItemPickup Prefab and sample Item

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -9,7 +9,8 @@ public class Item : ScriptableObject
     [SerializeField] private string id;
     [SerializeField] private string displayName;
     [TextArea] [SerializeField] private string description;
-
+    [SerializeField] private Sprite sprite;
+    public Sprite Sprite => sprite;
     public string Id => id;
     public string DisplayName => displayName;
     public string Description => description;
