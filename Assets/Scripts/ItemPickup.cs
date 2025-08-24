1387a9e20345cb133dee65b887fcb5f086913fb9
Timb9o <timo.patolla@hotmail.de>
2025-08-25 01:05:40 +0200

    Ready to merge

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -61,7 +61,8 @@ public class ItemPickup : MonoBehaviour
         ui?.RefreshInventory(inventory);
         ui?.ShowFlavourText(GetRandomResponse(successResponses) ?? $"Picked up {item.DisplayName}");
         onPickedUp?.Invoke();
-        SoundManager.Instance.PlaySFX(item.Sound);
+        
+        SoundManager.Instance?.PlaySFX(item.Sound);
         DestroyState.MarkDestroyed(GetId());
         Destroy(gameObject);
         return true;
